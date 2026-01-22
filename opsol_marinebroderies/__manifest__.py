2d7feacc7a374e463e385d4af998326c1e100f2d
Hamza YAYA <hamza.yaya121@gmail.com>
2026-01-22 12:41:49 +0100

    [ADD] Add aggregation value for quantity

--- opsol_marinebroderies/__manifest__.py
+++ opsol_marinebroderies/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'opsol_marinebroderies',
-    'version': '1.3.11',
+    'version': '1.3.12',
     'summary': 'Add custom modification for Marine Broderies',
     'description': 'Add custom modification for Marine Broderies',
     'category': 'Uncategorized',
