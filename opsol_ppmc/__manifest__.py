0a856e723437f93a6d89d673a14612f51163dbf0
Hamza YAYA <hamza.yaya121@gmail.com>
2026-06-26 20:00:16 +0100

    [IMP] Order payment by name

--- opsol_ppmc/__manifest__.py
+++ opsol_ppmc/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OPSol PPMC",
-    "version": "19.0.1.0.15",
+    "version": "19.0.1.0.16",
     "summary": "PPMC vendor payments report",
     "description": """
 PPMC vendor payments reports and layouts.
