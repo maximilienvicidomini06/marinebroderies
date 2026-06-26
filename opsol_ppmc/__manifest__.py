557661d273cc1e5e984811ad849d0667d6eb1f3e
Hamza YAYA <hamza.yaya121@gmail.com>
2026-06-26 20:18:42 +0100

    [FIX] fix url link for payment

--- opsol_ppmc/__manifest__.py
+++ opsol_ppmc/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "OPSol PPMC",
-    "version": "19.0.1.0.16",
+    "version": "19.0.1.0.17",
     "summary": "PPMC vendor payments report",
     "description": """
 PPMC vendor payments reports and layouts.
